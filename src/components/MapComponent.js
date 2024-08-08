7372174281b8ed45692ea5c727a6be162458eca1
patpatpanda <nils-emil1337@hotmail.se>
2024-08-08 10:52:49 +0200

    a

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -104,7 +104,7 @@ const MapComponent = () => {
 
     const loadScript = () => {
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCbJmqNnZHTZ99pPQ2uHfkDXwpMxOpfYLw&libraries=places`;
       script.async = true;
       script.defer = true;
       script.onload = () => initMap();

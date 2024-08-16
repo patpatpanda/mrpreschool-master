66966ad96543f1d4d4f221d11ad5d54df3d6b0b6
patpatpanda <nils-emil1337@hotmail.se>
2024-08-16 14:58:54 +0200

    aa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import MapComponent from './components/MapComponent';
 import SurveyChart from './components/SurveyChart';
 import Header from './components/Header'; // Importera Header-komponenten
 import theme from './components/theme';
-
 import './App.css';
+
+// Google Analytics integration
+import ReactGA from 'react-ga';
+
+function Analytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize('G-BB5BB4CLSJ'); // Ersätt med ditt Google Analytics-mätnings-ID
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
@@ -14,6 +27,7 @@ function App() {
       <Router>
         <div className="App">
           <Header /> {/* Lägg till Header-komponenten här */}
+          <Analytics /> {/* Lägg till Analytics-komponenten för att spåra sidvisningar */}
           <main>
             <Routes>
               <Route path="/" element={<MapComponent />} />

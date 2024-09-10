728aed29249abe2ce4e46256ca805c3b98760c8a
patpatpanda <nils-emil1337@hotmail.se>
2024-09-10 14:51:24 +0200

    a

--- src/components/StyledButton.js
+++ src/components/StyledButton.js
@@ -11,7 +11,7 @@ const StyledButton = styled(Button)(({ theme }) => ({
   zIndex: 9999,  // Hög z-index för att den ska synas över allt
   padding: '12px 24px',  // Ökad padding för en lyxigare känsla
   fontFamily: 'Nunito, sans-serif',  // Modern font
-  
+   border: '2px solid #333',
   borderRadius: '12px',  // Rundade hörn
   boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',  // Lättare skugga för att matcha den ljusa bakgrunden
   textTransform: 'none',  // Ingen versal text

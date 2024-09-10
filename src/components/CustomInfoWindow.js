9ab22d9fdad5ad72257cc2620303defae13a46f5
patpatpanda <nils-emil1337@hotmail.se>
2024-09-10 12:21:23 +0200

    a

--- src/components/CustomInfoWindow.js
+++ src/components/CustomInfoWindow.js
@@ -7,16 +7,7 @@ export class CustomInfoWindow extends google.maps.OverlayView {
   }
 
   onAdd() {
-    this.div = document.createElement('div');
-    this.div.style.position = 'absolute';
-    this.div.style.backgroundColor = 'white';
-    this.div.style.padding = '10px';
-    this.div.style.borderRadius = '5px';
-    this.div.style.boxShadow = '0 2px 6px rgba(0,0,0,0.3)';
-    this.div.style.transform = 'translate(-50%, -100%)';
-    this.div.style.whiteSpace = 'nowrap';
-    this.div.innerHTML = this.content;
-
+    
     const panes = this.getPanes();
     panes.floatPane.appendChild(this.div);
   }

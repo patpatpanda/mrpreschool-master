6fd9282b8befae55fcb4e1d881c3fee4d9fd001b
patpatpanda <nils-emil1337@hotmail.se>
2024-09-10 13:44:24 +0200

    a

--- src/components/DetailedCard.js
+++ src/components/DetailedCard.js
@@ -226,7 +226,7 @@ const DetailedCard = ({ schoolData, onClose }) => {
             position: 'absolute',
             right: { xs: 8, sm: 8 },
             top: { xs: 8, sm: 8 },
-            color: '#333',
+            color: '#fff',
           }}
         >
           <FontAwesomeIcon icon={faTimes} />

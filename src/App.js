1b67ad0e83c7e48cc0a2129c8a9c3dc5ec517a9d
patpatpanda <nils-emil1337@hotmail.se>
2024-09-10 11:12:50 +0200

    a

--- src/App.js
+++ src/App.js
@@ -40,26 +40,40 @@ function App() {
             </Routes>
           </main>
 
-          {/* Lägg till knappen i det övre högra hörnet */}
+          {/* Lägg till knappen i det övre vänstra hörnet */}
           <Button
             onClick={() => window.location.href = 'https://blog.xn--frskolekollen-imb.se/'}
             variant="contained"
             sx={{
               backgroundColor: '#ffffff',  // Vit bakgrund för icke-aktiv knapp
               color: '#333',  // Mörk textfärg för läsbarhet
-              position: 'fixed',  // Fixed position för att knappen ska vara i hörnet
-              top: '20px',  // Placering från toppen
-              right: '20px',   // Placering från högerkanten
+              position: 'absolute',  // Fixed position för att knappen ska vara i hörnet
+              top: '60px',  // Placering från toppen
+              left: '10px',   // Placering från vänsterkanten
               zIndex: 9999,  // Mycket högt z-index för att säkerställa att den visas över alla element
               padding: '10px 20px',
               fontFamily: 'Nunito, sans-serif',
               border: '2px solid #333',  // Fullständig definition av kantlinje
               borderRadius: '8px',  // Rundade hörn
               boxShadow: 'none',  // Ingen skugga för knappen
-              width: '120px',  // Fast bredd för alla skärmstorlekar
+              width: '80px',  // Fast bredd för mobiler
               fontSize: '0.85rem',  // Fast textstorlek
               '&:hover': {
                 backgroundColor: 'lightgrey',  // Ljusare bakgrund vid hover
+              },
+
+              // Anpassning för större skärmar med breakpoints
+              [theme.breakpoints.up('md')]: {
+                top: '40px',  // Flytta längre ned på större skärmar
+                right: '40px',  // Flytta till höger för större skärmar
+                width: '120px',  // Större bredd för knappen på större skärmar
+                fontSize: '1rem',  // Större textstorlek på större skärmar
+              },
+              [theme.breakpoints.up('lg')]: {
+                top: '60px',  // Ännu längre ned för mycket stora skärmar
+                right: '60px',  // Flytta längre bort från högerkanten
+                width: '140px',  // Ännu större bredd för mycket stora skärmar
+                fontSize: '1.1rem',  // Ännu större textstorlek
               },
             }}
           >

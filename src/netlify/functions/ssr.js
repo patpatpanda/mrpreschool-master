e8c6e6fae543b944ab9aee3f9fa5a53a907f6fc7
patpatpanda <nils-emil1337@hotmail.se>
2024-10-10 14:25:23 +0200

    s

--- src/netlify/functions/ssr.js
+++ src/netlify/functions/ssr.js
@@ -1,24 +1,24 @@
-// const React = require('react');
-// const ReactDOMServer = require('react-dom/server');
-// const App = require('../../src/App').default;  // Importera din App-komponent
-// const fs = require('fs');
-// const path = require('path');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const App = require('../../src/App').default;  // Importera din App-komponent
+const fs = require('fs');
+const path = require('path');
 
-// exports.handler = async function(event, context) {
-//   // Läs in index.html från din byggmapp
-//   const indexFile = fs.readFileSync(path.resolve(__dirname, '../../dist/index.html'), 'utf8');
+exports.handler = async function(event, context) {
+  // Läs in index.html från din byggmapp
+  const indexFile = fs.readFileSync(path.resolve(__dirname, '../../dist/index.html'), 'utf8');
   
-//   // Rendera React-appen som en HTML-sträng
-//   const appHtml = ReactDOMServer.renderToString(React.createElement(App));
+  // Rendera React-appen som en HTML-sträng
+  const appHtml = ReactDOMServer.renderToString(React.createElement(App));
 
-//   // Ersätt innehållet i <div id="root"></div> med din renderade React-app
-//   const html = indexFile.replace('<div id="root"></div>', `<div id="root">${appHtml}</div>`);
+  // Ersätt innehållet i <div id="root"></div> med din renderade React-app
+  const html = indexFile.replace('<div id="root"></div>', `<div id="root">${appHtml}</div>`);
 
-//   return {
-//     statusCode: 200,
-//     headers: {
-//       'Content-Type': 'text/html',
-//     },
-//     body: html,  // Returnera den server-renderade HTML:n
-//   };
-// };
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: html,  // Returnera den server-renderade HTML:n
+  };
+};
